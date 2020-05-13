6e72cc6b09bdfe40b9c6049f2911d80bceeb9ba9
lteixeirarocha <lucas.teixeirarocha@altran.com>
2020-05-13 18:02:59 +0100

    Adds appbar and image

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -2,8 +2,18 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Center(
-      child: Text("Hello World"),
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        title: Text("I Am Rich"),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: Center(
+        child: Image(
+          image: NetworkImage(
+              "https://image.shutterstock.com/image-photo/white-transparent-leaf-on-mirror-260nw-1029171697.jpg"),
+        ),
+      ),
     ),
   ));
 }
