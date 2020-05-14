4cd319f75d295bd4b990d460088a4f4975f0749b
lteixeirarocha <lucas.teixeirarocha@altran.com>
2020-05-14 16:02:37 +0100

    finish

--- i_am_poor/lib/main.dart
+++ i_am_poor/lib/main.dart
@@ -1,18 +1,48 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
-      backgroundColor: Colors.blue[200],
-      appBar: AppBar(
-        title: Text('I Am Poor'),
-        backgroundColor: Colors.orange,
-      ),
-      body: Center(
-        child: Image(
-          image: AssetImage('images/flame-payment-processed.png'),
-        ),
-      ),
-    ),
-  ));
+      home: Scaffold(
+          backgroundColor: Colors.blue[200],
+          appBar: AppBar(
+            title: Text('I Am Poor'),
+            backgroundColor: Colors.orange,
+          ),
+          body: Center(
+            child: RichText(
+              text: TextSpan(children: [
+                WidgetSpan(
+                    child: Image(
+                  image: AssetImage('images/flame-payment-processed.png'),
+                )),
+                WidgetSpan(
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                      RichText(text: TextSpan(text: 'illustration by ')),
+                      RichText(
+                          text: TextSpan(
+                        style: TextStyle(
+                            color: Colors.deepPurpleAccent,
+                            decoration: TextDecoration.underline),
+                        text: 'Ouch.pics',
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () async {
+                            final url = 'https://icons8.com';
+                            if (await canLaunch(url)) {
+                              await launch(
+                                url,
+                                forceSafariVC: false,
+                              );
+                            }
+                          },
+                      ))
+                    ]))
+              ]),
+            ),
+          ))));
 }
+//https://icons8.com
