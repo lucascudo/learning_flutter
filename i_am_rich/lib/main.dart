e455c265b747ff59ce8b88b4267310e2b8097fe4
lteixeirarocha <lucas.teixeirarocha@altran.com>
2020-05-14 11:08:22 +0100

    use AssetImage

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -5,13 +5,12 @@ void main() {
     home: Scaffold(
       backgroundColor: Colors.blueGrey,
       appBar: AppBar(
-        title: Text("I Am Rich"),
+        title: Text('I Am Rich'),
         backgroundColor: Colors.blueGrey[900],
       ),
       body: Center(
         child: Image(
-          image: NetworkImage(
-              "https://image.shutterstock.com/image-photo/white-transparent-leaf-on-mirror-260nw-1029171697.jpg"),
+          image: AssetImage('images/diamond.png'),
         ),
       ),
     ),
