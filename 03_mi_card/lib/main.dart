4620c9a12bd64be59f76a9947bd6f21cbb551beb
lteixeirarocha <lucas.teixeirarocha@altran.com>
2020-05-15 13:33:33 +0100

    finish lesson 3

--- 03_mi_card/lib/main.dart
+++ 03_mi_card/lib/main.dart
@@ -1,41 +1,120 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  static const String PHONE = '+351 937 513 604';
+  static const String EMAIL = 'lucasrochabr@outlook.com';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
+        appBar: AppBar(
+          title: Text('Business Card'),
+          backgroundColor: Colors.black,
+        ),
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Container(
-                width: 100,
-                color: Colors.red,
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('images/avatar.jpg'),
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.yellow,
+              Text(
+                'Lucas Rocha',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'Pacifico',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 40,
+                ),
+              ),
+              Text(
+                'Developer'.toUpperCase(),
+                style: TextStyle(
+                  color: Colors.teal[100],
+                  fontFamily: 'Source Sans Pro',
+                  fontSize: 20,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+                width: 200,
+                child: Divider(
+                  color: Colors.teal[900],
+                  thickness: 1.5,
+                ),
+              ),
+              GestureDetector(
+                onTap: () async {
+                  final url = 'tel:' + PHONE;
+                  if (await canLaunch(url)) {
+                    await launch(
+                      url,
+                      forceSafariVC: false,
+                    );
+                  }
+                },
+                child: Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 25,
                   ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.green,
-                  )
-                ],
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      PHONE,
+                      style: TextStyle(
+                        color: Colors.teal[900],
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ),
               ),
-              Container(
-                width: 100,
-                color: Colors.blue,
+              GestureDetector(
+                onTap: () async {
+                  final url = 'mailto:' + EMAIL;
+                  if (await canLaunch(url)) {
+                    await launch(
+                      url,
+                      forceSafariVC: false,
+                    );
+                  }
+                },
+                child: Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 25,
+                  ),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      EMAIL,
+                      style: TextStyle(
+                        color: Colors.teal[900],
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
