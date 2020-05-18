549376c3cc54b1de5ea26ccd5c4dcb2ed3fe21d2
lteixeirarocha <lucas.teixeirarocha@altran.com>
2020-05-18 13:19:51 +0100

    adds 05_xylophone

---
+++ 05_xylophone/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:audioplayers/audio_cache.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  final player = new AudioCache();
+
+  Expanded buildKey({Color color, int note}) => Expanded(
+        child: FlatButton(
+          child: null,
+          color: color,
+          onPressed: () => player.play('note$note.wav'),
+        ),
+      );
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildKey(color: Colors.red, note: 1),
+              buildKey(color: Colors.orange, note: 2),
+              buildKey(color: Colors.yellow, note: 3),
+              buildKey(color: Colors.green, note: 4),
+              buildKey(color: Colors.teal, note: 5),
+              buildKey(color: Colors.blue, note: 6),
+              buildKey(color: Colors.purple, note: 7),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
